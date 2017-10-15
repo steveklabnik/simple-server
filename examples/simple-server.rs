794c06f982b4f1cc1638dadc29e823699d4e68ff
steveklabnik <steve@steveklabnik.com>
2017-10-15 11:13:48 -0400

    Make the closure example the main example.
    Fixes #4

--- examples/simple-server.rs
+++ examples/simple-server.rs
@@ -1,18 +1,15 @@
 extern crate simple_server;
 
-use simple_server::{Server, Request, Response};
-use simple_server::response::Builder as ResponseBuilder;
+use simple_server::Server;
 
 fn main() {
+    let server = Server::new(|request, mut response| {
+        println!("Request received. {} {}", request.method(), request.uri());
+        response.body("Hello Rust!".as_bytes()).unwrap()
+    });
+
     let host = "127.0.0.1";
     let port = "7878";
 
-    fn handler(request: Request<&[u8]>, mut response_builder: ResponseBuilder) -> Response<&[u8]> {
-        println!("Request received. {} {}", request.method(), request.uri());
-        response_builder.body("Hello Rust!".as_bytes()).unwrap()
-    };
-
-    let server = Server::new(handler);
-
-    server.listen(host, port)
+    server.listen(host, port);
 }
