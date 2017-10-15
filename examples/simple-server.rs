8bb1f43dc94cf113cc1d070f043c990f898889f6
steveklabnik <steve@steveklabnik.com>
2017-10-15 12:18:18 -0400

    feat(handler): Introduce fallable handlers
    If your request fails, we return a 500 internal server error.
    
    This is useful so that things built on top can propogate errors if
    they don't care about handling them specifically, making the server
    overall more robust.

--- examples/simple-server.rs
+++ examples/simple-server.rs
@@ -3,13 +3,13 @@ extern crate simple_server;
 use simple_server::Server;
 
 fn main() {
-    let server = Server::new(|request, mut response| {
-        println!("Request received. {} {}", request.method(), request.uri());
-        response.body("Hello Rust!".as_bytes()).unwrap()
-    });
-
     let host = "127.0.0.1";
     let port = "7878";
+
+    let server = Server::new(|request, mut response| {
+        println!("Request received. {} {}", request.method(), request.uri());
+        Ok(response.body("Hello Rust!".as_bytes())?)
+    });
 
     server.listen(host, port);
 }
