034ebc182df596f6f032004faf125947d16e0d7b
Jacob Brown <kardeiz@gmail.com>
2017-10-19 12:20:02 -0500

    add example

---
+++ examples/dynamic_response.rs
@@ -0,0 +1,34 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+extern crate simple_server;
+
+use simple_server::{Server, Method, StatusCode};
+
+fn main() {
+    let host = "127.0.0.1";
+    let port = "7878";
+
+    let server = Server::new(|request, mut response| {
+        info!("Request received. {} {}", request.method(), request.uri());
+
+        match request.method() {
+            &Method::GET => {
+                let body = format!("The path you requested was '{}'", request.uri().path());
+                Ok(response.body(body.into_bytes())?)
+            }
+            &Method::POST => {
+                let data = String::from_utf8_lossy(request.body()).into_owned();
+                let body = format!("The data you posted was '{}'", data);
+                Ok(response.body(body.into_bytes())?)
+            }
+            _ => {
+                response.status(StatusCode::NOT_FOUND);
+                Ok(response.body(b"<h1>404</h1><p>Not found!<p>".to_vec())?)
+            }
+        }
+    });
+
+    server.listen(host, port);
+}
